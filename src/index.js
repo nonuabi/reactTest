a8e7d2afae11410c4f6ae2a56d52d837e009662c
nonuabi <nonuabi@gmail.com>
2021-06-28 23:39:51 +0530

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-
+//dom
 ReactDOM.render(
   <React.StrictMode>
     <App />
